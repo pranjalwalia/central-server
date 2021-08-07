bd354634dfcad7a146be5fc1cd9af16fb53031b2
Chayan <55481820+Chayan-19@users.noreply.github.com>
2021-08-07 18:15:50 +0530

    Update stackoverflow.js
    Added comments to better describe the functionality.

--- src/libs/stackoverflow.js
+++ src/libs/stackoverflow.js
@@ -1,5 +1,7 @@
+//importing axios for API call to the external API
 const axios = require("axios");
 
+//API call
 const getTopAnswer = async (searchKey) => {
   var queryLink =
     "https://api.stackexchange.com/2.3/search/advanced?pagesize=5&order=desc&sort=votes&site=stackoverflow&q=" +
@@ -8,7 +10,7 @@ const getTopAnswer = async (searchKey) => {
   try {
     const { data } = await axios.get(queryLink);
     const response = data?.items;
-    const filterResponse = response.map((res) => ({
+    const filterResponse = response.map((res) => ({    /*filtering title and link from the response and mapping it to the array of objects*/
       title: res.title,
       link: res.link,
     }));
@@ -19,6 +21,7 @@ const getTopAnswer = async (searchKey) => {
   }
 };
 
+//regular expression to replace all the single space character with the `+` character to build the query
 function queryBuilder(searchKey) {
   return searchKey.replace(/ /g, "+");
 }
