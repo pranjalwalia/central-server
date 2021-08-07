3298a00ca02de1b52079a5c26c5514c808fd6b5e
Chayan <55481820+Chayan-19@users.noreply.github.com>
2021-08-07 18:26:43 +0530

    Update youtube.js
    Added comments to better describe functionality.

--- src/libs/youtube.js
+++ src/libs/youtube.js
@@ -1,7 +1,11 @@
+//importing the youtube token and the google API
+//IMP NOTE: importing axios is not needed when working with "googleapis" as it has built in support of axios
 const { google } = require("googleapis");
 const { YOUTUBE_TOKEN } = require("../config");
-const url = "https://youtube.com/watch?v=";
+const url = "https://youtube.com/watch?v=";//this will be used to append the video id at the end for the response
 
+
+//API call
 const getResults = async (searchKey) => {
   try {
     const res = await google.youtube("v3").search.list({
@@ -10,6 +14,7 @@ const getResults = async (searchKey) => {
       q: `${searchKey}`,
     });
 
+    //using optional chaining to filter title, channel name, and video url from the response
     const data = res?.data?.items?.map((item) => ({
       title: item.snippet.title,
       channel: item.snippet.channelTitle,
@@ -22,6 +27,7 @@ const getResults = async (searchKey) => {
     return [];
   }
 
+  //replacing all occurrences of single space with `+` to build the query
   function queryBuilder(searchKey) {
     return searchKey.replace(/ /g, "+");
   }
